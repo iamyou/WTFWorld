cda987afbc0d1de83b7b3a74855b2a991962da5c
leoweyr <2516427163@qq.com>
2022-03-06 15:51:19 +0800

    rebuild FrameLibPoint.cpp

--- SDK/source/FrameLibPoint/FrameLibPoint.cpp
+++ SDK/source/FrameLibPoint/FrameLibPoint.cpp
@@ -21,22 +21,24 @@ DLLEXPORT BOOL dllPath(std::string dllName,std::string *dllPath){
 }
 */
 
-BOOL dllPath(std::string dllName,std::string *dllPath) {
+BOOL dllPath(char* dllName,char *dllPath) {
     WindowsRegistry::Op registryRead;
     if (registryRead.OpenKey("SOFTWARE\\OdysseyWorld\\FolderPoint") == false) {
         return false;
     } else {
-        if (registryRead.Read("a1b1", dllPath) == false) {
+        std::string dllName_str = dllName;
+        std::string dllPath_str;
+        if (registryRead.Read("a1b1", dllPath_str) == false) {
             return false;
         } else {
-            *dllPath = *dllPath + "\\" + dllName + ".dll";
+            dllPath_str = dllPath_str + "\\" + dllName_str + ".dll";
+            std::cout << dllPath_str << std::endl;
             return true;
         }
     }
 }
 int main(){
-    char *dllPath_;
+    char* dllPath_;
     dllPath("FolderPoint",dllPath_);
-    std::cout << "3." << *dllPath_ << std::endl;
     return 0;
 }
