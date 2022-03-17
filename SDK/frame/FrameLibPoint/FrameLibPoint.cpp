a3a6959ec2bd376dd231838eaf3ea45a237e7345
leoweyr <2516427163@qq.com>
2022-03-17 20:50:26 +0800

    Divided into three development routes, they are app, frame and odk

---
+++ SDK/frame/FrameLibPoint/FrameLibPoint.cpp
@@ -0,0 +1,43 @@
+#define DLLEXPORT extern "C" __declspec(dllexport)
+/*
+#include <iostream>
+#include <string>
+*/
+#include "../WindowsRegistry/WindowsRegistry.h"
+
+/*
+DLLEXPORT BOOL dllPath(std::string dllName,std::string *dllPath){
+    WindowsRegistry::Op registryRead;
+    if (registryRead.OpenKey("SOFTWARE\\OdysseyWorld\\FolderPoint") == false){
+        return false;
+    }else{
+        if (registryRead.Read("a1b1",dllPath) == false){
+            return false;
+        }else{
+            *dllPath = *dllPath + "\\" + dllName + ".dll";
+            return true;
+        }
+    }
+}
+*/
+
+
+BOOL dllPath(std::string dllName,std::string *dllPath){
+    WindowsRegistry::Op registryRead;
+    if (registryRead.OpenKey("SOFTWARE\\OdysseyWorld\\FolderPoint") == false){
+        return false;
+    }else{
+        if (registryRead.Read("a1b1",dllPath) == false){
+            return false;
+        }else{
+            *dllPath = *dllPath + "\\" + dllName + ".dll";
+            return true;
+        }
+    }
+}
+
+int main(){
+    char dllPath_;
+    dllPath("FolderPoint",()dllPath_);
+    return 0;
+}
